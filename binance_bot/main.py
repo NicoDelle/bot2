751b6597e7aec9bfd3aaaf2b199297a16dd7f368
NicoDelle <nicodelle04@gmail.com>
2022-06-13 13:57:22 +0200

    first plot made

--- binance_bot/main.py
+++ binance_bot/main.py
@@ -1,4 +1,10 @@
 import db_creator
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 db = db_creator.db_constructor()
+
+fig, ax = plt.subplots()
+ax.plot(db.index, db.open)
+plt.show()
